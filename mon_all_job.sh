e015d896f0c4b0e1745d4c4c8ee72cefba454cc3
mutate-github <mutate@mail.ru>
2026-02-12 23:40:44 +0300

    mon_all_job.sh - new version with timing jobs

--- mon_all_job.sh
+++ mon_all_job.sh
@@ -26,100 +26,47 @@ if [[ -n "$etime" ]] && [[ ! "$etime" =~ "00:0[0123]" ]]; then
    exit 127
 fi
 
+start_job_with_timing()
+{
+(
+local script="$1"
+START=$(date +%s)
+$BASEDIR/${script} $CLIENT >> $BASEDIR/../log/${script}.log 2>&1
+END=$(date +%s)
+DURATION=$((END - START))
+# echo "Monitor: ================= mon_ping.sh "$(date +%d/%m/%Y-%H:%M:%S)  
+printf "%-15s Duration: %3d s %-10s %-20s \n" "${script}" "$DURATION" " Finish_time: " "$(date +%d/%m/%Y-%H:%M:%S)"
+) 
+}
+
+
 echo ""
 echo "START ALL MONITORING *****************************************************************************"$(date)
-echo "Monitor: ================================================================================ mon_ping.sh "$(date)
-$BASEDIR/mon_ping.sh $CLIENT >> $BASEDIR/../log/mon_ping.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_ssh.sh "$(date)
-$BASEDIR/mon_ssh.sh $CLIENT >> $BASEDIR/../log/mon_ssh.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_db.sh "$(date)
-$BASEDIR/mon_db.sh $CLIENT >> $BASEDIR/../log/mon_db.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_tnslsnr.sh "$(date)
-$BASEDIR/mon_tnslsnr.sh $CLIENT >> $BASEDIR/../log/mon_tnslsnr.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_url.sh "$(date)
-$BASEDIR/mon_url.sh $CLIENT >> $BASEDIR/../log/mon_url.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_port.sh "$(date)
-$BASEDIR/mon_port.sh $CLIENT >> $BASEDIR/../log/mon_port.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_fs.sh "$(date)
-$BASEDIR/mon_fs.sh $CLIENT >> $BASEDIR/../log/mon_fs.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_disksp.sh "$(date)
-$BASEDIR/mon_disksp.sh $CLIENT >> $BASEDIR/../log/mon_disksp.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_swap.sh "$(date)
-$BASEDIR/mon_swap.sh $CLIENT >> $BASEDIR/../log/mon_swap.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_load.sh "$(date)
-$BASEDIR/mon_load.sh $CLIENT >> $BASEDIR/../log/mon_load.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_alert.sh "$(date)
-$BASEDIR/mon_alert.sh $CLIENT >> $BASEDIR/../log/mon_alert.sh.log 2>&1 &
-sleep 1
-#$BASEDIR/mon_adrcial.sh $CLIENT
-echo "Monitor: ================================================================================ mon_fra.sh "$(date)
-$BASEDIR/mon_fra.sh $CLIENT >> $BASEDIR/../log/mon_fra.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_tbs.sh "$(date)
-echo "Monitor: ================================================================================ mon_fs.sh "$(date)
-$BASEDIR/mon_fs.sh $CLIENT >> $BASEDIR/../log/mon_fs.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_disksp.sh "$(date)
-$BASEDIR/mon_disksp.sh $CLIENT >> $BASEDIR/../log/mon_disksp.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_swap.sh "$(date)
-$BASEDIR/mon_swap.sh $CLIENT >> $BASEDIR/../log/mon_swap.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_load.sh "$(date)
-$BASEDIR/mon_load.sh $CLIENT >> $BASEDIR/../log/mon_load.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_alert.sh "$(date)
-$BASEDIR/mon_alert.sh $CLIENT >> $BASEDIR/../log/mon_alert.sh.log 2>&1 &
-sleep 1
-#$BASEDIR/mon_adrcial.sh $CLIENT
-echo "Monitor: ================================================================================ mon_fra.sh "$(date)
-$BASEDIR/mon_fra.sh $CLIENT >> $BASEDIR/../log/mon_fra.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_tbs.sh "$(date)
-$BASEDIR/mon_tbs.sh $CLIENT >> $BASEDIR/../log/mon_tbs.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_stb.sh "$(date)
-$BASEDIR/mon_stb.sh $CLIENT >> $BASEDIR/../log/mon_stb.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_bck.sh "$(date)
-$BASEDIR/mon_bck.sh $CLIENT >> $BASEDIR/../log/mon_bck.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_db_files.sh "$(date)
-$BASEDIR/mon_db_files.sh $CLIENT >> $BASEDIR/../log/mon_db_files.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_reslim.sh "$(date)
-$BASEDIR/mon_reslim.sh $CLIENT >> $BASEDIR/../log/mon_reslim.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_resumab.sh "$(date)
-$BASEDIR/mon_resumab.sh $CLIENT >> $BASEDIR/../log/mon_resumab.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_pga.sh "$(date)
-$BASEDIR/mon_pga.sh $CLIENT >> $BASEDIR/../log/mon_pga.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_lock.sh "$(date)
-$BASEDIR/mon_lock.sh $CLIENT >> $BASEDIR/../log/mon_lock.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_forlog.sh "$(date)
-$BASEDIR/mon_forlog.sh $CLIENT >> $BASEDIR/../log/mon_forlog.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_aas.sh "$(date)
-$BASEDIR/mon_aas.sh $CLIENT >> $BASEDIR/../log/mon_aas.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ mon_oratop.sh "$(date)
-$BASEDIR/mon_oratop.sh $CLIENT >> $BASEDIR/../log/mon_oratop.sh.log 2>&1 &
-sleep 1
-echo "Monitor: ================================================================================ kill_sniped.sh "$(date)
-$BASEDIR/kill_sniped.sh $CLIENT >> $BASEDIR/../log/mon_kill_sniped.sh.log 2>&1 &
+start_job_with_timing mon_ping.sh &
+start_job_with_timing mon_ssh.sh &
+start_job_with_timing mon_db.sh &
+start_job_with_timing mon_tnslsnr.sh &
+start_job_with_timing mon_url.sh &
+start_job_with_timing mon_port.sh &
+start_job_with_timing mon_fs.sh &
+start_job_with_timing mon_disksp.sh &
+start_job_with_timing mon_swap.sh &
+start_job_with_timing mon_load.sh &
+start_job_with_timing mon_alert.sh &
+# start_job_with_timing mon_adrcial.sh &
+start_job_with_timing mon_fra.sh &
+start_job_with_timing mon_tbs.sh &
+start_job_with_timing mon_stb.sh &
+start_job_with_timing mon_bck.sh &
+start_job_with_timing mon_db_files.sh &
+start_job_with_timing mon_reslim.sh &
+start_job_with_timing mon_resumab.sh &
+start_job_with_timing mon_pga.sh &
+start_job_with_timing mon_lock.sh &
+start_job_with_timing mon_forlog.sh &
+start_job_with_timing mon_aas.sh &
+start_job_with_timing mon_oratop.sh &
+start_job_with_timing kill_sniped.sh &
 wait
 echo "FINISH ALL MONITORING ****************************************************************************"$(date)
 
